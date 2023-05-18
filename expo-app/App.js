b5e9d99493bf95aceae17081c3d5c6373393a36f
Jennifer Español <ackermanjennifer19@gmail.com>
2023-05-18 20:00:03 +0800

    Machine Problem 3

--- expo-app/App.js
+++ expo-app/App.js
@@ -1,20 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { View, StyleSheet, Alert, Text } from "react-native";
+import { TextInput, Button } from "@react-native-material/core";
 
-export default function App() {
+const App = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = () => {
+    if (username === "admin" && password === "password") {
+      Alert.alert("Login successful!");
+    } else {
+      Alert.alert("Invalid credentials");
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput
+        label="Username"
+        value={username}
+        onChangeText={(text) => setUsername(text)}
+        style={styles.input}
+      />
+      <TextInput
+        label="Password"
+        value={password}
+        onChangeText={(text) => setPassword(text)}
+        secureTextEntry
+        style={styles.input}
+      />
+      <Button title="Login" style={styles.button} color="#009688" onPress={handleLogin} />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 16,
+  },
+  input: {
+    width: "100%",
+    marginBottom: 12,
+  },
+  button: {
+    width: "100%",
+    marginTop: 12,
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "bold",
   },
 });
+
+export default App;
